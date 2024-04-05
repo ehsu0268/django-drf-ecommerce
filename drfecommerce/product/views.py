6502ea3d3afdd86776066544b44d0112507b464b
ehsu0268 <31189322+ehsu0268@users.noreply.github.com>
2024-04-05 11:19:39 -0400

    Adding changes to support ProductLine model (#3)

--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Category, Brand, Product
 from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 from drf_spectacular.utils import extend_schema
+from django.db import connection
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import SqlLexer
+from sqlparse import format
 
 
 # Create your views here.
@@ -33,14 +39,42 @@ class BrandViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
 
-class ProductiewSet(viewsets.ViewSet):
+class ProductViewSet(viewsets.ViewSet):
     """
     A simpel Viewset for viewing all products
     """
 
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().isactive()
+    # queryset = Product.objects.all()
+    lookup_field = "slug"
+
+    def retrieve(self, request, slug=None):
+        serializer = ProductSerializer(self.queryset.filter(slug=slug), many=True)
+        # serializer = ProductSerializer(
+        #     self.queryset.filter(slug=slug).select_related("category", "brand"),
+        #     many=True,
+        # )
+        # data = Response(serializer.data)
+        #
+        # q = list(connection.queries)
+        # print(len(q))
+        # for qs in q:
+        #     sqlformatted = format(str(qs["sql"]), reindent=True)
+        #     print(highlight(sqlformatted, SqlLexer(), TerminalFormatter()))
+        data = Response(serializer.data)
+        return data
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(methods=["get"], detail=False, url_path=r"category/(?P<slug>[\w-]+)")
+    def list_product_by_category_slug(self, request, slug=None):
+        """
+        An endpoint to return products by category
+        """
+        serializer = ProductSerializer(
+            self.queryset.filter(category__slug=slug), many=True
+        )
         return Response(serializer.data)
